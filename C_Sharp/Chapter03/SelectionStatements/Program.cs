47ff36e4502ad9220530a9abb741ab214e534a77
KudaChan <kumarnchandan05@gmail.com>
2024-03-13 23:08:54 +0530

    todays progress in C#

--- C_Sharp/Chapter03/SelectionStatements/Program.cs
+++ C_Sharp/Chapter03/SelectionStatements/Program.cs
@@ -58,3 +58,60 @@ A_label:
 WriteLine($"After A_label");
 
 #endregion
+
+#region Pattern matching with the switch statement
+
+var animals = new Animal?[]
+{
+    new Cat { Name = "Karen", Born = new(year: 2022, month: 8, day: 23), Legs = 4, IsDomestic = true},
+    null,
+    new Cat { Name = "Mufasa", Born = new(year: 1994, month: 6, day: 12) },
+    new Spider { Name = "Sid Vicious", Born = DateTime.Today, IsPoisonous = true },
+    new Spider { Name = "Captain Furry", Born = DateTime.Today }
+};
+
+foreach (Animal? animal in animals)
+{
+    string message;
+
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has 4 legs.";
+            break;
+
+        case Cat WildCat when WildCat.IsDomestic == false:
+            message = $"The cat named {WildCat.Name} is wild.";
+            break;
+
+        case Cat cat:
+            message = $"The cat named is {cat.Name}.";
+            break;
+
+        case Spider spider when spider.IsPoisonous:
+            message = $"{spider.Name} is a poisonous spider.";
+            break;
+
+        case null:
+            message = "There is no animal to identify";
+            break;
+
+        default:
+            message = $"{animal.Name} is a {animal.GetType().Name}.";
+            break;
+    }
+    WriteLine($"Switch statement: {message}");
+
+    message = animal switch
+    {
+        Cat fourLeggedCat when fourLeggedCat.Legs == 4 => $"The cat named {fourLeggedCat.Name} has 4 legs.",
+        Cat WildCat when WildCat.IsDomestic == false => $"The cat named {WildCat.Name} is wild.",
+        Cat cat => $"The cat named is {cat.Name}.",
+        Spider spider when spider.IsPoisonous => $"{spider.Name} is a poisonous spider.",
+        null => "There is no animal to identify",
+        _ => $"{animal.Name} is a {animal.GetType().Name}."
+    };
+    WriteLine($"switch expression: {message}");
+}
+
+#endregion
--- C_Sharp/Chapter03/SelectionStatements/obj/Debug/net8.0/SelectionStatements.AssemblyInfo.cs
+++ C_Sharp/Chapter03/SelectionStatements/obj/Debug/net8.0/SelectionStatements.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb17989aef15c76314bb8d9e073ad7145f592ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b9b9eb8fb17d1d1d2529452f9490ddf2d577b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
