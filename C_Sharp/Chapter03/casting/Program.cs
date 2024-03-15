1da58b35985aa7d7ca5d4deaf6ec3a66246d08ec
KudaChan <kumarnchandan05@gmail.com>
2024-03-15 21:56:35 +0530

    status on C# learning

--- C_Sharp/Chapter03/casting/Program.cs
+++ C_Sharp/Chapter03/casting/Program.cs
@@ -1,2 +1,52 @@
-﻿WriteLine("Hello, Casting!");
+﻿using static System.Convert;
+
+WriteLine("Hello, Casting!");
 WriteLine("");
+
+#region casting implicitly and explicitly
+
+int a = 10;
+double b = a;
+WriteLine($"a is {a}, b is {b}");
+
+WriteLine("");
+
+double c = 9.8;
+int d = (int)c;
+WriteLine($"c is {c}, d is {d}");
+
+WriteLine("");
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+#endregion casting implicitly and explicitly
+
+WriteLine("");
+
+#region Negative numbers representation in binary
+
+WriteLine("{0,12} {1, 34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i > -8; i--)
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+#endregion Negative numbers representation in binary
+
+WriteLine("");
+
+#region Convrting with the System.Convert type
+
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g is {g}, h is {h}");
+
+#endregion Convrting with the System.Convert type
--- C_Sharp/Chapter03/casting/obj/Debug/net8.0/casting.AssemblyInfo.cs
+++ C_Sharp/Chapter03/casting/obj/Debug/net8.0/casting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("casting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ff36e4502ad9220530a9abb741ab214e534a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6341ccf820c7e4c54c823a0bd0b1c17daac49472")]
 [assembly: System.Reflection.AssemblyProductAttribute("casting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("casting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
