471e8ad49b294cb095009953d15b7af6e65307fb
KudaChan <kumarnchandan05@gmail.com>
2024-05-28 06:28:07 +0530

    todays progress

--- C_Sharp/MicrosoftLearn/chapter3/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ C_Sharp/MicrosoftLearn/chapter3/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e853b51a4859cc7ddecb7d46db48025b4a8e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07472021c1ce5f6f24f01cd4cef3af44c338a659")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
