9c1e1f8301172e3a825b2c20e9926f563fd05ca0
kumar <kumarnchandan05@gmail.com>
2024-06-02 07:46:09 +0530

    todays work

--- bash/bash_scripting/project/countdown.sh
+++ bash/bash_scripting/project/countdown.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Program that counts down to zero from a given argument
 
-echo -e "\n~~ Countdown Timer ~~\n" 
+echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1 -gt 0 ]]
 then
