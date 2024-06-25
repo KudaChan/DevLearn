741a2b565cfc76974044646e926402d962349862
kumar <kumarnchandan05@gmail.com>
2024-06-26 01:08:37 +0530

    feat: add rock-paper-scissor project

---
+++ language/JS/freecodecamp/06_rock_paper_scissor/script.js
@@ -0,0 +1,77 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const index = Math.floor(Math.random() * options.length);
+
+    return options[index];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+    return ((player === "Rock" && computer === "Scissors") || (player === "Scissors" && computer === "Paper") || (player === "Paper" && computer === "Rock"))
+}
+
+let playerScore = 0;
+let computerScore = 0;
+function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+    if (userOption === computerResult) {
+        return (`It's a tie! Both chose ${userOption}`);
+    } else if (hasPlayerWonTheRound(userOption, computerResult)) {
+        playerScore++;
+        return (`Player wins! ${userOption} beats ${computerResult}`);
+    } else {
+        computerScore++;
+        return (`Computer wins! ${computerResult} beats ${userOption}`);
+    }
+}
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    if (playerScore === 3 || computerScore === 3) {
+        if (computerScore === 3) {
+            winnerMsgElement.innerText = "Computer has won the game!";
+        } else {
+            winnerMsgElement.innerText = "Player has won the game!";
+        }
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+};
+
+resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+});
+
