9e90a65d1b7a004a047c05f251d7517803633c0d
kumar <kumarnchandan05@gmail.com>
2024-07-04 01:07:28 +0530

    feat: add todo app (completed)

--- language/JS/freecodecamp/11_todo_app/script.js
+++ language/JS/freecodecamp/11_todo_app/script.js
@@ -1,19 +1,16 @@
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
-
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
-
 const tasksContainer = document.getElementById("tasks-container");
-
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
@@ -24,17 +21,21 @@ const addOrUpdateTask = () => {
         date: dateInput.value,
         description: descriptionInput.value,
     };
+
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     } else {
         taskData[dataArrIndex] = taskObj;
     }
+
+    localStorage.setItem("data", JSON.stringify(taskData));
     updateTaskContainer();
     reset();
 };
 
 const updateTaskContainer = () => {
     tasksContainer.innerHTML = "";
+
     taskData.forEach(
     ({ id, title, date, description }) => {
         (tasksContainer.innerHTML += `
@@ -42,42 +43,58 @@ const updateTaskContainer = () => {
                 <p><strong>Title:</strong> ${title}</p>
                 <p><strong>Date:</strong> ${date}</p>
                 <p><strong>Description:</strong> ${description}</p>
-                <button type="button" class="btn">Edit</button>
-                <button type="button" class="btn">Delete</button>
+                <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+                <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
             </div>
         `)
-    });
+        }
+    );
 };
 
+
 const deleteTask = (buttonEl) => {
-    const dataArrIndex = taskData.findIndex((item) =>
-        item.id === buttonEl.parentElement.id);
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
     buttonEl.parentElement.remove();
     taskData.splice(dataArrIndex, 1);
+    localStorage.setItem("data", JSON.stringify(taskData));
 };
 
 const editTask = (buttonEl) => {
-    const dataArrIndex = taskData.findIndex((item) =>
-        item.id === buttonEl.parentElement.id);
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
     currentTask = taskData[dataArrIndex];
+
     titleInput.value = currentTask.title;
     dateInput.value = currentTask.date;
     descriptionInput.value = currentTask.description;
+
     addOrUpdateTaskBtn.innerText = "Update Task";
+
     taskForm.classList.toggle("hidden");
 };
 
 const reset = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
     titleInput.value = "";
     dateInput.value = "";
     descriptionInput.value = "";
-    currentTask = {};
-
     taskForm.classList.toggle("hidden");
+    currentTask = {};
 };
 
-openTaskFormBtn.addEventListener("click", () => taskForm.classList.toggle("hidden")
+if (taskData.length) {
+    updateTaskContainer();
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+    taskForm.classList.toggle("hidden")
 );
+
 closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
     const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
@@ -88,7 +105,9 @@ closeTaskFormBtn.addEventListener("click", () => {
         reset();
     }
 });
+
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
 discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
     reset();
@@ -96,5 +115,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", e => {
     e.preventDefault();
+
     addOrUpdateTask();
 });
