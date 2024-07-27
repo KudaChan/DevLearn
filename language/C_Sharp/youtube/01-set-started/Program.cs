4a5d572143615ea86dffca49b535e917d570dfef
Chandan Kumar <kumarnchandan05@gmail.com>
2024-07-27 22:55:23 +0530

    regular commit

---
+++ language/C_Sharp/youtube/01-set-started/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_set_started
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+// Output: Hello World!
---
+++ language/C_Sharp/youtube/02-data-types-variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_data_types_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
